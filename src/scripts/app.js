02271eaa491c1e95a524d6f1f81936cd90139738
evawolf <eva-wolf@gmx.net>
2021-05-04 17:36:54 +0200

    finished TO-DOs refactored html/scss added JavaScript Code for logo animation

---
+++ src/scripts/app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function (event) {
+  setTimeout(function () {
+    var v = document.getElementById('header-content')
+    v.className += ' -revealed'
+  }, 4000)
+})
